6fdafce99a1e387be259c6db9b6b7135df0ce41a
Andhi <agungandhita@gmail.com>
2023-06-18 18:00:42 +0700

    baru

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -75,11 +75,12 @@
                             <h1>Total Anggota</h1>
                         </div>
 
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-20 opacity-50" viewBox="0 0 448 512">
-                            <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                            <path
-                                d="M219.3 .5c3.1-.6 6.3-.6 9.4 0l200 40C439.9 42.7 448 52.6 448 64s-8.1 21.3-19.3 23.5L352 102.9V160c0 70.7-57.3 128-128 128s-128-57.3-128-128V102.9L48 93.3v65.1l15.7 78.4c.9 4.7-.3 9.6-3.3 13.3s-7.6 5.9-12.4 5.9H16c-4.8 0-9.3-2.1-12.4-5.9s-4.3-8.6-3.3-13.3L16 158.4V86.6C6.5 83.3 0 74.3 0 64C0 52.6 8.1 42.7 19.3 40.5l200-40zM111.9 327.7c10.5-3.4 21.8 .4 29.4 8.5l71 75.5c6.3 6.7 17 6.7 23.3 0l71-75.5c7.6-8.1 18.9-11.9 29.4-8.5C401 348.6 448 409.4 448 481.3c0 17-13.8 30.7-30.7 30.7H30.7C13.8 512 0 498.2 0 481.3c0-71.9 47-132.7 111.9-153.6z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-24 opacity-50" viewBox="0 0 24 24" style="fill:currentcolor">
+                            <path d="M12 2a5 5 0 1 0 5 5 5 5 0 0 0-5-5zm0 8a3 3 0 1 1 3-3 3 3 0 0 1-3 3zm9 11v-1a7 7 0 0 0-7-7h-4a7 7 0 0 0-7 7v1h2v-1a5 5 0 0 1 5-5h4a5 5 0 0 1 5 5v1z"></path>
                         </svg>
+
+ 
+
                     </div>
 
                     <a href=""
--- resources/views/admin/ranting/index.blade.php
+++ resources/views/admin/ranting/index.blade.php
@@ -1,24 +1,74 @@
 @extends('admin.layout.three')
 
 @section('container')
-<div class="px-4 pt-6">
-    {{-- card-1 start --}}
+    <div class="px-4 pt-6">
+        {{-- card-1 start --}}
 
 
 
-        <div
-            class="p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 sm:p-6 dark:bg-gray-800">
+        <div class="p-4 bg-slate-700 rounded-lg shadow-sm dark:border-gray-700 sm:p-6 dark:bg-gray-800">
             <!-- Card header -->
             <div class="items-center justify-between lg:flex">
-                <div class="mb-4 lg:mb-0">
-                    <h3 class="mb-2 text-xl font-bold text-gray-900 dark:text-white">Profile</h3>
-                </div>
-            </div>
-            <!-- Table -->
-
-        {{-- table admin end --}}
+                <div class="mx-4 min-h-screen w-full sm:mx-8 xl:mx-auto">
+                    <h1 class="pb-2 text-4xl font-semibold dark:text-gray-300">Profile ranting</h1>
 
 
-    </div>
-</div>
-@endsection
+
+                    <div class="col-span-8 overflow-hidden rounded-xl sm:px-8 sm:shadow py-10">
+                        <div class="pt-4">
+                            @if (session('success'))
+                                <div class="alert bg-green-200 rounded-lg py-5 px-6 mb-8 text-base text-black inline-flex items-center w-full alert-dismissible fade show "
+                                    role="alert">
+                                    <strong class="mr-1">{{ session('success') }}</strong>
+                                    <button type="button"
+                                        class="btn-close box-content w-4 h-4 p-1 ml-auto text-yellow-900 border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-yellow-900 hover:opacity-75 hover:no-underline"
+                                        data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>
+                            @endif
+
+                            @if (session('error'))
+                                <div class="alert bg-green-200 rounded-lg py-5 px-6 mb-8 text-base text-white inline-flex items-center w-full alert-dismissible fade show "
+                                    role="alert">
+                                    <strong class="mr-1">{{ session('error') }}</strong>
+                                    <button type="button"
+                                        class="btn-close box-content w-4 h-4 p-1 ml-auto text-yellow-900 border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-yellow-900 hover:opacity-75 hover:no-underline"
+                                        data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>
+                            @endif
+
+                        </div>
+                        <hr class="mt-4 mb-8" />
+                        <form method="post" action="/profile/update" enctype="multipart/form-data">
+                            <img src="" alt="" id="view_image"
+                                class="w-20 h-20 object-cover flex justify-center m-auto rounded-full">
+                            <div class="flex flex-col mx-auto my-4 justify-center gap-y-4 mb-4">
+                                <input type="file" name="image" class=" mx-auto w-[10%] rounded-md" id="image">
+                                <label for="image" class="text-center font-semibold">Ubah Foto Profile</label>
+                            </div>
+
+                            <div class="grid grid-cols-2 gap-8">
+                                <div>
+                                    <label for="username">Username</label>
+                                    <input type="text" name="username" class="w-full h-10 rounded-md mt-2">
+                                </div>
+                                <div>
+                                    <label for="email">Email</label>
+                                    <input type="text" name="email" class="w-full h-10 rounded-md mt-2">
+                                </div>
+                                <div>
+                                    <label for="no_tlpn">No Telephone</label>
+                                    <input type="text" name="no_tlpn" class="w-full h-10 rounded-md mt-2">
+                                </div>
+                                <div>
+                                    <label for="alamat">Alamat</label>
+                                    <input type="text" name="alamat" class="w-full h-10 rounded-md mt-2">
+                                </div>
+                            </div>
+
+                            <button type="submit" class="bg-orange-600 text-white p-2 rounded-md mt-8">Simpan</button>
+
+                        </form>
+
+
+                    </div>
+                @endsection
