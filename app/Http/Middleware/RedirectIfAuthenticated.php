2345d097bf14cca77b2eaa5b7fb371005574c001
Ryan Yulianto <88651368+Lemperbro@users.noreply.github.com>
2023-11-02 00:08:17 +0700

    Update RedirectIfAuthenticated.php

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -23,7 +23,19 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                
+                if(Auth()->user() !== null){
+                    if(Auth()->user()->role == 'admin'){
+                        return redirect('/admin');
+                    }elseif(Auth()->user()->role == 'cabang'){
+                        return redirect('/cabang');
+                    }elseif(Auth()->user()->role == 'ranting'){
+                        return redirect('/ranting');
+                    }else{        
+                    return redirect(RouteServiceProvider::HOME);
+                    }
+                 }
+                
             }
         }
 
