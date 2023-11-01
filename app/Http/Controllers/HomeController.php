c323774f27338df367f4ff1da6c3b7c429ed7f9c
Ryan Yulianto <88651368+Lemperbro@users.noreply.github.com>
2023-11-01 23:46:58 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,6 +14,16 @@ class HomeController extends Controller
         if(request('nia')){
             $data = Anggota::where('nia',request('nia'))->first();
         }
+
+        if(Auth()->user() !== null){
+            if(Auth()->user()->role == 'admin'){
+                return redirect('/admin');
+            }elseif(Auth()->user()->role == 'cabang'){
+                return redirect('/cabang');
+            }elseif(Auth()->user()->role == 'ranting'){
+                return redirect('/ranting');
+            }
+        }
         return view('data.cari', [
             'data' => $data
         ]);
