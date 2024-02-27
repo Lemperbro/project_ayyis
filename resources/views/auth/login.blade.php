9b8e124a3ff95bc4b8d1fe5794d1787d793ec3c3
ryan <yuliantoryan64@gmail.com>
2024-02-27 18:20:37 +0700

    sdsd

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
 
                 <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white mt-2">
-                    Login
+                    Loginn
                 </h1>
 
                 @if (session('UpdateSuccess'))
