99b19024507e4130cb1bfab8629f23c46d845515
ryan <yuliantoryan64@gmail.com>
2024-03-15 05:12:56 +0700

    as

--- app/Console/Commands/DbBackup.php
+++ app/Console/Commands/DbBackup.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
 use Illuminate\Support\Facades\Response;
 
 class DbBackup extends Command
@@ -27,18 +28,31 @@ class DbBackup extends Command
      * @return int
      */
     public function handle()
-    {
-        // Nama file backup
-        $filename = 'backup_' . now()->timestamp . '.sql';
-    
-        // Command untuk membuat backup
-        $command = 'mysqldump --user=' . env('DB_USERNAME') . ' --password=' . env('DB_PASSWORD') . ' --host=' . env('DB_HOST') . ' ' . env('DB_DATABASE');
-    
-        // Set header untuk memicu unduhan file
-        header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename="' . $filename . '"');
-    
-        // Eksekusi perintah untuk membuat backup dan langsung kirimkan hasilnya ke output
-        passthru($command);
+{
+    // Nama file backup
+    $filename = 'backup_' . now()->timestamp . '.sql';
+
+    // Command untuk membuat backup
+    $command = 'mysqldump --user=' . env('DB_USERNAME') . ' --password=' . env('DB_PASSWORD') . ' --host=' . env('DB_HOST') . ' ' . env('DB_DATABASE');
+
+    // Membuat proses
+    $process = Process::fromShellCommandline($command);
+
+    // Memulai proses
+    $process->start();
+
+    // Set header untuk memicu unduhan file
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="' . $filename . '"');
+
+    // Membaca output dan mengirimkan ke output HTTP
+    while ($process->isRunning()) {
+        echo $process->getOutput();
+        flush();
+        usleep(100000); // Jeda 0.1 detik
     }
+
+    // Mengakhiri proses
+    $process->stop();
+}
 }
