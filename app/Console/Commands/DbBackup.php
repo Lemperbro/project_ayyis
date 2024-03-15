2fee5dc3aea544d93f245c14293241f4e404e84b
ryan <yuliantoryan64@gmail.com>
2024-03-15 12:05:54 +0700

    as

--- app/Console/Commands/DbBackup.php
+++ app/Console/Commands/DbBackup.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Storage;
 
 class DbBackup extends Command
 {
@@ -27,9 +29,24 @@ class DbBackup extends Command
      */
     public function handle()
     {
-        $filename = 'backup_'.strtotime(now()).'.sql';
-        $command = 'mysqldump --user='.env('DB_USERNAME').' --password='.env('DB_PASSWORD').' --host='.env('DB_HOST').' '.env('DB_DATABASE').' > '.public_path('backup/'.$filename);
-    
+        $filename = 'backup_' . strtotime(now()) . '.sql';
+        $command = 'mysqldump --user=' . env('DB_USERNAME') . ' --password=' . env('DB_PASSWORD') . ' --host=' . env('DB_HOST') . ' ' . env('DB_DATABASE') . ' > ' . storage_path() . '/app/backup/' . $filename;
+
         exec($command);
+
+        // Dapatkan output dari perintah backup
+        $output = Artisan::output();
+
+        // Nama file backup
+
+        // Simpan output backup ke file sementara
+        Storage::put('/app/backup/' . $filename, $output);
+
+        // Set header untuk memicu unduhan file
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="' . $filename . '"');
+
+        // Baca file dan kirimkan ke output HTTP
+        readfile(Storage::path('/app/backup/' . $filename));
     }
 }
